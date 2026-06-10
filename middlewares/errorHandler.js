06c9bb4f6e3f8142c7477361dc33563b13e15b66
Mithilesh Watane <mithilesh.kanakasoftware@gmail.com>
2026-06-10 23:11:22 +0530

    Erro Handling

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,13 +2,11 @@ const errorHandler = (err, req, res, next) => {
     let statusCode = err.statusCode || 500;
     let message = err.message || "Something went wrong";
 
-    // Handle Mongoose cast errors (invalid ObjectIds)
     if (err.name === 'CastError') {
         statusCode = 400;
         message = `Invalid ${err.path}: ${err.value}`;
     }
 
-    // Handle Mongoose validation errors
     if (err.name === 'ValidationError') {
         statusCode = 400;
         message = Object.values(err.errors).map((val) => val.message).join('. ');
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { userController } from "../controllers/userController.js";
 import validateBody from "../middlewares/validateBody.js";
-import { createUserSchema, updateUserSchema } from "../validators/userValidator.js";
+import { createUserSchema, updateUserSchema } from "../zodSchema/zodSchema.js";
 
 const router = express.Router();
 const { createUser, getUser, getAllUsers, updateUser, deleteUser } = userController;
